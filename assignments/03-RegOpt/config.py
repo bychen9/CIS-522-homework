8740538a8e3f009766f56d7bd971ca35fa089d13
Brian Chen <bychen9@gmail.com>
2023-02-19 13:28:07 -0500

    Submit homework 3

--- assignments/03-RegOpt/config.py
+++ assignments/03-RegOpt/config.py
@@ -4,18 +4,21 @@ import torch.optim
 import torch.nn as nn
 from torchvision.transforms import Compose, ToTensor
 
-# from torchvision.transforms import Compose, Normalize, ToTensor
-
 
 class CONFIG:
     batch_size = 64
-    num_epochs = 10
+    num_epochs = 20
     initial_learning_rate = 0.001
     initial_weight_decay = 0
 
     lrs_kwargs = {
         # You can pass arguments to the learning rate scheduler
         # constructor here.
+        "num_batches": 600,
+        "num_epochs": num_epochs,
+        "initial_learning_rate": initial_learning_rate,
+        "eta_max": 0.003,
+        "gamma": 0.98,
     }
 
     optimizer_factory: Callable[
--- assignments/03-RegOpt/scheduler.py
+++ assignments/03-RegOpt/scheduler.py
@@ -1,15 +1,21 @@
 from typing import List
-
-from torch.optim.lr_scheduler import _LRScheduler
 import numpy as np
+from torch.optim.lr_scheduler import _LRScheduler
 
 
 class CustomLRScheduler(_LRScheduler):
-    """
-    A Custom Learning Rate Scheduler
-    """
+    """constructs a scheduler"""
 
-    def __init__(self, optimizer, last_epoch=-1):
+    def __init__(
+        self,
+        optimizer,
+        num_batches,
+        num_epochs,
+        initial_learning_rate,
+        eta_max,
+        gamma,
+        last_epoch=-1,
+    ):
         """
         Create a new scheduler.
 
@@ -18,16 +24,40 @@ class CustomLRScheduler(_LRScheduler):
 
         """
         # ... Your Code Here ...
+        self.optimizer = optimizer
+        self.last_epoch = last_epoch
+        self.num_batches = num_batches
+        self.num_epochs = num_epochs
+        self.total_steps = num_batches * self.num_epochs
+        self.eta_max = eta_max
+        self.gamma = gamma
+        self.last_lr = initial_learning_rate
+        self.t0 = self.total_steps / 5.0
         super(CustomLRScheduler, self).__init__(optimizer, last_epoch)
 
     def get_lr(self) -> List[float]:
-        """
-        Arguments: None
-        Returns: A list with learning rates
-        """
-
+        """gets learning rate"""
         # Note to students: You CANNOT change the arguments or return type of
         # this function (because it is called internally by Torch)
 
         # ... Your Code Here ...
-        return [0.002 / (1 + 0.001 * self.last_epoch)]
+        # Here's our dumb baseline implementation:
+        # if self.last_epoch % 300 == 0:
+        #    self.last_lr = self.last_lr * self.gamma
+        #    for op_params in self.optimizer.param_groups:
+        #        op_params['lr'] = self.last_lr
+        # -----------------------------------------
+        if self.last_epoch < self.t0:
+            self.last_lr = 10e-4 + self.eta_max * self.last_epoch / self.t0
+        if self.last_epoch < self.total_steps:
+            self.last_lr = (
+                self.eta_max
+                * np.cos(
+                    (np.pi / 2)
+                    * (self.last_epoch - self.t0)
+                    / (self.total_steps - self.t0)
+                )
+                + 10e-6
+            )
+        return [self.last_lr]
+        # return [i for i in self.base_lrs]
