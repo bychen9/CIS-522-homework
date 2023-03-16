62cc2917ceac60b182afc3e2f46d61b6862b017d
Brian Chen <bychen9@gmail.com>
2023-03-16 15:10:11 -0400

    Submit HW4

--- assignments/04-Convs/model.py
+++ assignments/04-Convs/model.py
@@ -9,12 +9,12 @@ class Model(torch.nn.Module):
 
     def __init__(self, num_channels: int, num_classes: int) -> None:
         super(Model, self).__init__()
-        self.conv2d1 = nn.Conv2d(num_channels, 8, 3)
-        self.conv2d2 = nn.Conv2d(num_channels, 8, 3)
+        self.conv2d1 = nn.Conv2d(num_channels, 10, 3)
+        self.conv2d2 = nn.Conv2d(num_channels, 10, 3)
 
         self.pool = nn.MaxPool2d(2)
         self.relu = nn.ReLU()
-        self.fc = nn.Linear(8 * 15 * 15 * 2, num_classes)
+        self.fc = nn.Linear(10 * 15 * 15 * 2, num_classes)
 
     def forward(self, x: torch.Tensor) -> torch.Tensor:
         """
